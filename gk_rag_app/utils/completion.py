588cd1b291be740350afa9078f5a252bbe1821c1
kapoordcu <gaurav.kapoor23@outlook.com>
2026-07-08 00:19:47 +0530

    missing python-dotenv will no longer crash Streamlit Cloud:

--- gk_rag_app/utils/completion.py
+++ gk_rag_app/utils/completion.py
@@ -1,7 +1,10 @@
 from google import genai
 import os
-from dotenv import load_dotenv
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ModuleNotFoundError:
+    pass
 
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 def generate_completion(prompt):
--- gk_rag_app/utils/embedding.py
+++ gk_rag_app/utils/embedding.py
@@ -1,8 +1,11 @@
 import os
 import requests
 import numpy as np
-from dotenv import load_dotenv
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ModuleNotFoundError:
+    pass
 
 JINA_API_KEY = os.getenv("JINA_API_KEY")
 
